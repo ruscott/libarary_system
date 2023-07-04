08ee4c61ff4ba35d034b962fce883d5596f3b37f
Ruchika Scott <ru.scott24@gmail.com>
2023-07-04 09:06:14 +0100

    initial commit

---
+++ LibrarySystem/Models/Book.cs
@@ -0,0 +1,10 @@
+namespace LibrarySystem.Models
+{
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public bool IsAvailable { get; set; }
+}
+}
---
+++ LibrarySystem/Models/Library.cs
@@ -0,0 +1,19 @@
+namespace LibrarySystem.Models
+{
+
+public class Library
+{
+    private List<Book> books;
+
+    public Library()
+    {
+        books = new List<Book>();
+    }
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+
+}
+}
